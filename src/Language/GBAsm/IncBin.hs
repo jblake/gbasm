5463b4c5759ba98f16cac6e9564139cc0922164f
Julian Blake Kongslie <jblake@jblake.org>
2013-08-27 15:09:08 -0700

    Catch exceptions in Inc handling, so we get better error messages.

--- src/Language/GBAsm/IncBin.hs
+++ src/Language/GBAsm/IncBin.hs
@@ -6,6 +6,7 @@
 module Language.GBAsm.IncBin
 where
 
+import Control.Exception
 import Data.ByteString.Lazy as BS
 import Data.Generics.Uniplate.Operations
 
@@ -18,11 +19,12 @@ incBinPass = transformM pass
   where
 
     pass (Inc dec name (Abs offset) (Abs len)) = do
-      fdata <- BS.readFile name
-      let fdata' = BS.take (fromIntegral len) $ BS.drop (fromIntegral offset) fdata
-      if BS.length fdata' /= fromIntegral len
-        then return $ Err dec $ "The file " ++ show name ++ ", starting at offset " ++ show offset ++ ", does not have at least " ++ show len ++ " bytes."
-        else return $ Bin dec fdata'
+      flip catch (\e -> return $ Err dec $ "The file " ++ show name ++ " could not be read: " ++ show (e :: SomeException)) $ do
+        fdata <- BS.readFile name
+        let fdata' = BS.take (fromIntegral len) $ BS.drop (fromIntegral offset) fdata
+        if BS.length fdata' /= fromIntegral len
+          then return $ Err dec $ "The file " ++ show name ++ ", starting at offset " ++ show offset ++ ", does not have at least " ++ show len ++ " bytes."
+          else return $ Bin dec fdata'
 
     pass (Inc dec name offset len) = return $ Err dec $ "Unable to resolve offset " ++ prettyOperand offset ++ " and/or length " ++ prettyOperand len ++ " to constants for inclusion of file " ++ show name ++ "."
 
